602dfe1ac01edf8235b2a2704bc1f898eb85f4ac
Raphael kiteme <raphael.kituku@gmail.com>
2024-06-04 07:20:31 -0400

    Updated js file

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
