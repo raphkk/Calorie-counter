8500ee72e1db0ae9f2953115838b673195433014
Raphael kiteme <raphael.kituku@gmail.com>
2024-06-12 06:48:07 -0400

    updated js file

--- script.js
+++ script.js
@@ -52,3 +52,17 @@ function calculateCalories(e) {
   if (isError) {
     return;
   }
+  
+  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  <p>${budgetCalories} Calories Budgeted</p>
+  <p>${consumedCalories} Calories Consumed</p>
+  <p>${exerciseCalories} Calories Burned</p>
+  `;
+
+  output.classList.remove('hide');
+}
